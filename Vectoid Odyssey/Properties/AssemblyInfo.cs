40a99feeb5a19af9e3e8f9e67d16aea3de161f98
Sixten Schelin <sixten.schelin@learnet.se>
2019-05-20 21:23:24 +0200

    hgfdhgfd

--- Vectoid Odyssey/Properties/AssemblyInfo.cs
+++ Vectoid Odyssey/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f47627af-e5b0-4f71-a2c8-ce34ed1aa7a9")]
